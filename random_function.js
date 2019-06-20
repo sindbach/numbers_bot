78d6f4693bdd2a241d9955e6bdc6e94b47ad97df
Wan Bachtiar <sindbach@gmail.com>
2019-06-20 16:28:19 +1000

    one wild

--- random_function.js
+++ random_function.js
@@ -1,21 +1,22 @@
 exports = function(arg){
   let collection = context.services.get("mongodb-atlas").db("numbers").collection("oz");
   let result = []; 
-  const lengthOfResult = 7;
-  
+  const lengthOfResult = 6;
+  const maxManual = 40;
   collection.findOne({})
     .then(doc => {
       if(doc) {
-        console.log(`Successfully found document: ${doc}.`);
         let numbers = doc.numbers;
         let picked = 0;
         for (let i=0; i<lengthOfResult; i++){
-          console.log(result);
           while(result.indexOf(picked) > -1 || picked===0){
               picked = numbers[Math.floor(Math.random() * numbers.length)];
           }
           result.push(picked);
         }
+        // One manual wild card number
+        result.push(Math.floor(Math.random() * Math.floor(maxManual)));
+        console.log(result);
         result = result.sort(function(a, b){return a - b});
       } else {
         console.error('Failed to find document in collection');
