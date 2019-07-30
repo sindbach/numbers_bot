e7bc3ecbe4edc213c91015da1881b857b9981f0e
Wan Bachtiar <sindbach@gmail.com>
2019-07-30 11:07:51 +1000

    Just random numbers

--- random_function.js
+++ random_function.js
@@ -1,28 +1,15 @@
 exports = function(arg){
-  let collection = context.services.get("mongodb-atlas").db("numbers").collection("oz");
   let result = []; 
-  const lengthOfResult = 6;
+  const lengthOfResult = 7;
   const maxManual = 40;
-  collection.findOne({})
-    .then(doc => {
-      if(doc) {
-        let numbers = doc.numbers;
-        let picked = 0;
-        for (let i=0; i<lengthOfResult; i++){
-          while(result.indexOf(picked) > -1 || picked===0){
-              picked = numbers[Math.floor(Math.random() * numbers.length)];
-          }
-          result.push(picked);
-        }
-        // One manual wild card number
-        result.push(Math.floor(Math.random() * Math.floor(maxManual)));
-        console.log(result);
-        result = result.sort(function(a, b){return a - b});
-      } else {
-        console.error('Failed to find document in collection');
-      }
-    })
-    .catch(err => console.error('Failed to find document: ${err}'));
-    
+  let picked = 0;
+  for (let i=0; i<lengthOfResult; i++){
+    while(result.indexOf(picked) > -1 || picked===0){
+        picked = Math.floor(Math.random() * Math.floor(maxManual));
+    }
+    result.push(picked);
+  }
+  result = result.sort(function(a, b){return a - b});
+  console.log(result);
   return result;
 };
